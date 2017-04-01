e4fa97e86fba7c5ed8010a36d2077cad51a0ba59
Oldev13 <sh13max@yandex.ru>
2017-04-02 00:23:38 +0800

    Add files via upload

---
+++ src/main.php
@@ -0,0 +1,39 @@
+<img src="src/img/WorldFactory.png" class="mainLogo"/>
+<div class="page">
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+</div>
+
+
+<script>
+
+if(documet.body.scrollHeight>100){
+	
+	documet.getElementByClassName('Logo').style.display = 'block';
+	
+}
+
+</script>
